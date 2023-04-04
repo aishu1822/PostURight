0ff152e3ba85eb95981fb5b3d44eb7fa97a819e0
Aishwarya Joshi <aishwarj@andrew.cmu.edu>
2023-04-03 21:36:32 -0400

    separate registration button, fix android google services json

--- posturight/lib/title.dart
+++ posturight/lib/title.dart
@@ -4,6 +4,7 @@ import 'package:posturight/create_account.dart';
 import 'colors.dart';
 import 'login.dart';
 import 'style.dart';
+import 'registration_button.dart';
 
 class TitleScreen extends StatelessWidget {
 
@@ -22,90 +23,27 @@ class TitleScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                          height: 94,//getVerticalSize(94),
-                          width: 265,//getHorizontalSize(265),
-                          margin: const EdgeInsets.only(top: 29,),//getMargin(top: 29),
-                          child: Stack(
-                              alignment: Alignment.bottomCenter,
-                              children: [
-                                Align(
-                                    alignment: Alignment.topCenter,
-                                    child: Text("PostURight",
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: TextAlign.left,
-                                        style: Style.txtPalanquinDarkRegular42)),
-                                Align(
-                                    alignment: Alignment.bottomCenter,
-                                    child: Text("Lorem Ipsum sit dolor amet",
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: TextAlign.left,
-                                        style: Style.txtSFProRegular22))
-                              ])),
-                      const Spacer(),
-                      SizedBox(
-                        height: 56,
-                        child: OutlinedButton(
-                            onPressed: () { 
-                              Navigator.push(context,
-                                MaterialPageRoute(builder: (context) => CreateAccountScreen()));
-                             },
-                            child: const Text("Get Started"),
-                        ),
-                      ),
+                        alignment: Alignment.topCenter,
+                        child: Text("PostURight",
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
+                            style: Style.txtPalanquinDarkRegular42)),
+                      const SizedBox(height: 15,),
                       Container(
-                          width: double.maxFinite,
-                          child: Container(
-                              margin: const EdgeInsets.only(top: 19,),//getMargin(top: 19),
-                              padding: const EdgeInsets.only(top: 29,),//getPadding(all: 13),
-                              decoration: Style.outlineBluegray600.copyWith(borderRadius: BorderRadius.circular(4,)),//BorderRadiusStyle.roundedBorder4),
-                              child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Padding(
-                                        padding: const EdgeInsets.only(top: 5,),//getPadding(top: 5),
-                                        child: RichText(
-                                            text: const TextSpan(children: [
-                                              TextSpan(
-                                                  text:"Already have an account?",
-                                                  style: TextStyle(
-                                                      color: Color.fromARGB(255, 0x2b, 0x79, 0x74),//ColorConstant.blueGray600,
-                                                      fontSize: 20,//getFontSize(20),
-                                                      fontFamily: 'Helvetica Neue',
-                                                      fontWeight:FontWeight.w500,
-                                                      letterSpacing: 0.4,//getHorizontalSize(0.4)
-                                                  )
-                                              ),
-                                              TextSpan(
-                                                  text: " ",
-                                                  style: TextStyle(
-                                                      color: Color.fromARGB(255, 0x2b, 0x79, 0x74),
-                                                      fontSize: 20,//getFontSize(20),
-                                                      fontFamily:'Helvetica Neue',
-                                                      fontWeight:FontWeight.w400,
-                                                      letterSpacing:0.4)),//getHorizontalSize(0.4))),
-                                              TextSpan(
-                                                  text: "Log in",
-                                                  style: TextStyle(
-                                                      color: Color.fromARGB(255, 0x2b, 0x79, 0x74),
-                                                      fontSize: 20,//getFontSize(20),
-                                                      fontFamily:'Helvetica Neue',
-                                                      fontWeight:FontWeight.w700,
-                                                      letterSpacing:0.4,))//getHorizontalSize(0.4)))
-                                            ]),
-                                            textAlign: TextAlign.left)),
-                                            ElevatedButton(
-                                              onPressed: (){
-                                                Navigator.push(context,
-                                                  MaterialPageRoute(builder: (context) => LoginScreen()));
-                                                // callback(LoginScreen());
-                                              }, 
-                                              child: Text("Login"),
-                                            )
-                                  ]
-                                )
-                              )
-                            )
-                    ]))));
+                          alignment: Alignment.bottomCenter,
+                          child: Text("Helping you fix your posture...",
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 18),
+                          )),
+                      const Padding(padding: EdgeInsets.only(top: 150)),
+                      registrationButton("Get Started", context, "register"),
+                      const SizedBox(height: 15,),
+                      registrationButton("Already have an account? Log in", context, "login"),
+                    ]
+                  )
+                )
+              )
+            );
   }
 }
