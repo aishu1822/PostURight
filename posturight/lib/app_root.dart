5100e2462a36d6f0b02492f82a3347a2587c2ef8
Aishwarya Joshi <aishwarj@andrew.cmu.edu>
2023-04-06 00:36:05 -0400

    refactor to move timer and bluetooth connect code from home.dart to app_root.dart

--- posturight/lib/app_root.dart
+++ posturight/lib/app_root.dart
@@ -4,10 +4,16 @@ import 'alert_settings.dart';
 import 'exercises.dart';
 import 'profile.dart';
 import 'home.dart';
+import 'dart:async';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'dart:typed_data';
+
+Timer? postureNotifTimer;
 
 class AppRoot extends StatefulWidget {
-  const AppRoot({Key? key}) : super(key: key);
-
+  AppRoot({Key? key}) : super(key: key);
+  
+  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
   @override
   State<AppRoot> createState() => AppRootState();
 }
@@ -19,12 +25,37 @@ class AppRootState extends State<AppRoot> {
   late Widget _currentWidget;//Registration1Screen();
   //WelcomeScreen();//TitleScreen();//Container();
 
+  List<int> accXchar=[], accYchar=[], accZchar=[], gyrXchar=[], gyrYchar=[], gyrZchar=[];
+  String _current_posture = "nothing yet";
+  List<BluetoothService> _services = <BluetoothService>[];
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  List<BluetoothDevice> devicesList = <BluetoothDevice>[];
+  BluetoothDevice? _connectedDevice = null;
+
 
   @override
   void initState() {
     super.initState();
     Widget titleScreen = HomePage(title: 'Home');//TitleScreen(callback: refresh);
     _currentWidget = titleScreen;
+
+    flutterBlue.connectedDevices
+        .asStream()
+        .listen((List<BluetoothDevice> devices) {
+      for (BluetoothDevice device in devices) {
+        _addDeviceTolist(device);
+      }
+    });
+    flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results) {
+        _addDeviceTolist(result.device);
+      }
+    });
+    flutterBlue.startScan();
+
+    postureNotifTimer = Timer.periodic(const Duration(seconds:1),(arg) {
+      checkPosture();
+    });
   }
 
   void refresh(Widget widget) {
@@ -41,11 +72,223 @@ class AppRootState extends State<AppRoot> {
       case 3: return setState(() => _currentWidget = AlertSettingsPage(title: 'Alert Settings'));    
     }
   }  
- 
-  @override
-  Widget build(BuildContext context) {
 
-    return MaterialApp (
+  double interpretValue(List<int> data) {
+    if (data.length < 4) return 0;
+    final bytes = Uint8List.fromList(data);
+    final byteData = ByteData.sublistView(bytes);
+    double value = byteData.getFloat32(0,Endian.little);
+    print(value); 
+    return value;
+  }
+
+  void checkPosture() {
+    if (_connectedDevice == null) return;
+
+    // BluetoothService serv = _services[2]; // assuming 3 services and IMU service is the third one
+    // assert(serv.uuid.toString().split('-')[0] == '00001101');
+    
+    print("timer thread");
+    
+    double accX = interpretValue(widget.readValues[accX_uuid]!);
+    double accY = interpretValue(widget.readValues[accY_uuid]!);
+    double accZ = interpretValue(widget.readValues[accZ_uuid]!);
+    double gyrX = interpretValue(widget.readValues[gyrX_uuid]!);
+    double gyrY = interpretValue(widget.readValues[gyrX_uuid]!);
+    double gyrZ = interpretValue(widget.readValues[gyrX_uuid]!);
+
+    print("Accelerometer: accX = $accX, accY = $accY, accZ = $accZ");
+    print("Gyroscope: gyrX = $gyrX, gyrY = $gyrY, gyrZ = $gyrZ");
+
+    if (accZ > 0.3) {
+      setState(() {
+        _current_posture = "slouching!";
+      });
+    } else {
+      if (_current_posture == "slouching!" || _current_posture == "nothing yet") {
+        setState(() {
+        _current_posture = "straight!";
+      });
+      }
+    }    
+  }
+
+  _addDeviceTolist(final BluetoothDevice device) {
+   if (!devicesList.contains(device)) {
+     setState(() {
+       devicesList.add(device);
+     });
+   }
+ }
+
+ List<ButtonTheme> _buildReadWriteNotifyButton(BluetoothCharacteristic characteristic) {
+   List<ButtonTheme> buttons = <ButtonTheme>[];
+ 
+   if (characteristic.properties.read) {
+     buttons.add(
+       ButtonTheme(
+         minWidth: 10,
+         height: 20,
+         child: Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 4),
+           child: ElevatedButton(
+             child: Text('READ', style: TextStyle(color: Colors.white)),
+             onPressed: () async {
+               var sub = characteristic.value.listen((value) {
+                 setState(() {
+                   widget.readValues[characteristic.uuid] = value;
+                 });
+               });
+               await characteristic.read();
+               sub.cancel();
+             },
+           ),
+         ),
+       ),
+     );
+   }
+
+   if (characteristic.properties.notify) {
+     buttons.add(
+       ButtonTheme(
+         minWidth: 10,
+         height: 20,
+         child: Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 4),
+           child: ElevatedButton(
+             child: Text('NOTIFY', style: TextStyle(color: Colors.white)),
+             onPressed: () async {
+              characteristic.value.listen((value) {
+                widget.readValues[characteristic.uuid] = value;
+              });
+              await characteristic.setNotifyValue(true);
+             },
+           ),
+         ),
+       ),
+     );
+   }
+ 
+   return buttons;
+ }
+
+ ListView _buildConnectDeviceView() {
+  List<Container> containers = <Container>[];
+ 
+   for (BluetoothService service in _services) {
+     List<Widget> characteristicsWidget = <Widget>[];
+     for (BluetoothCharacteristic characteristic in service.characteristics) {
+       characteristicsWidget.add(
+         Align(
+           alignment: Alignment.centerLeft,
+           child: Column(
+             children: <Widget>[
+               Row(
+                 children: <Widget>[
+                   Text(characteristic.uuid.toString(), style: TextStyle(fontWeight: FontWeight.bold)),
+                 ],
+               ),
+               Row(
+                 children: <Widget>[
+                   ..._buildReadWriteNotifyButton(characteristic),
+                 ],
+               ),
+             ],
+           ),
+         ),
+       );
+     }
+     containers.add(
+       Container(
+         child: ExpansionTile(
+             title: Text(service.uuid.toString()),
+             children: characteristicsWidget),
+       ),
+     );
+   }
+ 
+   return ListView(
+     padding: const EdgeInsets.all(8),
+     children: <Widget>[
+       ...containers,
+     ],
+   );
+ }
+
+ ListView _buildListViewOfDevices() {
+   List<Container> containers = <Container>[];
+   for (BluetoothDevice device in devicesList) {
+    //  print(device.id);
+    //  print(device.name);
+     if (device.id.toString() != "78:21:84:AA:36:56" || device.name.toString() != "PostURight Sensor") continue;
+     containers.add(
+       Container(
+         height: 50,
+         child: Row(
+           children: <Widget>[
+             Expanded(
+               child: Column(
+                 children: <Widget>[
+                   Text(device.name == '' ? '(unknown device)' : device.name),
+                   Text(device.id.toString()),
+                 ],
+               ),
+             ),
+             ElevatedButton(
+               child: Text(
+                 'Connect',
+                 style: TextStyle(color: Colors.white),
+               ),
+               onPressed: () async {
+                List<BluetoothService> new_services;
+                List<BluetoothCharacteristic> bclist;
+                flutterBlue.stopScan();
+                  try {
+                    await device.connect();
+                  } catch (e) {
+                    // if (e.code != 'already_connected') {
+                    //   throw e;
+                    // }
+                    print(e);
+                  } finally {
+                    new_services = await device.discoverServices();
+                    // TODO: remove idx hard coding
+                    bclist = new_services[2].characteristics; 
+                    for (BluetoothCharacteristic c in bclist) {
+                      c.value.listen((value) {
+                        widget.readValues[c.uuid] = value;
+                      });                      
+                      await c.setNotifyValue(true);
+                    }
+                    setState(() {
+                      _services = new_services;
+                      _connectedDevice = device; 
+                    });
+
+                    // Navigator.pushAndRemoveUntil(context,
+                    //   MaterialPageRoute(builder: (context) => AppRoot()),
+                    //   (Route<dynamic> route) => false,
+                    // );
+                  }
+                  
+               },
+             ),
+           ],
+         ),
+       ),
+     );
+   }
+ 
+   return ListView(
+     padding: const EdgeInsets.all(8),
+     children: <Widget>[
+       ...containers,
+     ],
+   );
+ }
+
+ Widget appRootWidget() {
+  return MaterialApp (
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 182, 224, 207).withOpacity(1),
         body: _currentWidget,
@@ -79,5 +322,23 @@ class AppRootState extends State<AppRoot> {
         ),
       )
     );
+ }
+
+ Widget _buildView() {
+   if (_connectedDevice != null) {
+     return appRootWidget();//_buildConnectDeviceView();
+   }
+   return Scaffold(
+       appBar: AppBar(
+         title: Text("Posture status: " + _current_posture),//Text(widget.title),
+       ),
+       body:_buildListViewOfDevices(),
+      );
+ }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildView();    
   }
 }
